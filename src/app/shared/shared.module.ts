5adea511842d288eb0492b88a09c53e6f0d2be25
Cineva <cinevapp@gmail.com>
2025-10-09 11:13:40 +0200

    ajustes layout

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { NavbarComponent } from './components/navbar/navbar.component';
+// import { ProductCardComponent } from './components/product-card/product-card.component';
+
+// @NgModule({
+//   declarations: [
+//     NavbarComponent,
+//     ProductCardComponent
+//   ],
+//   imports: [
+//     CommonModule
+//   ]
+// })
+// export class SharedModule { }
