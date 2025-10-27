4eedb715bc9cbe42fb9e362b902a26c3f2d3dee2
Juanpedrogomezespinosa <juanpedrogomezespinosa@gmail.com>
2025-10-27 19:00:59 +0100

    ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,32 @@
 // src/app/app.component.ts
-
 import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
+import { RouterOutlet, Router } from "@angular/router";
 import { ToastComponent } from "./shared/components/toast/toast.component";
+import { NavbarComponent } from "./shared/components/navbar/navbar.component";
+import { FooterComponent } from "./shared/components/footer/footer.component";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, ToastComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ToastComponent,
+    NavbarComponent,
+    FooterComponent,
+  ],
   templateUrl: "./app.component.html",
   styleUrls: [],
 })
 export class AppComponent {
   title = "footer-front";
+
+  constructor(private router: Router) {}
+
+  // Propiedad para saber si estamos en login o register
+  get hideLayout(): boolean {
+    const currentRoute = this.router.url;
+    return currentRoute.includes("login") || currentRoute.includes("register");
+  }
 }
