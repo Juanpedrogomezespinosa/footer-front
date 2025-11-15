4c0ae1aab7a9130d74119c8e3fa7ebfd56adfd19
Juanpedrogomezespinosa <juanpedrogomezespinosa@gmail.com>
2025-11-15 17:17:36 +0100

    footer modificado

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,7 +1,11 @@
 import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common"; // Para usar {{ currentYear }}
+import { RouterLink } from "@angular/router"; // Para usar [routerLink]
 
 @Component({
   selector: "app-footer",
+  standalone: true, // Asumo que este también es standalone
+  imports: [CommonModule, RouterLink], // Añadir CommonModule y RouterLink
   templateUrl: "./footer.component.html",
   styleUrls: [],
 })
